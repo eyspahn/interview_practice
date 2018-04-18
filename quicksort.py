8a58f680cdac7b516abf8dee23a9d106bef2954a
Emily Spahn <eyspahn@gmail.com>
2018-04-17 19:33:44 -0700

    cleaning up folders

--- quicksort.py
+++
@@ -1,9 +0,0 @@
-"""Implement quick sort in Python.
-Input a list.
-Output a sorted list."""
-def quicksort(array):
-    return []
-
-
-# test = [21, 4, 1, 3, 9, 20, 25, 6, 21, 14]
-# print quicksort(test)
